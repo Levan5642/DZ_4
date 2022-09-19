1be1c311c7fb31326ec71229429abe161e979ae4
Levan5642 <91324000+Levan5642@users.noreply.github.com>
2022-09-19 16:34:59 +0300

    keke

---
+++ DZ_4/src/main/java/com/geek/utils/InputHelper.java
@@ -0,0 +1,85 @@
+package com.geek.utils;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+
+public class InputHelper {
+
+    private static final DataInputStream in =
+            new DataInputStream(System.in);
+
+    private static final int MAXLEN = 255;
+
+    private static String inputString() throws IOException {
+        byte buf[] = new byte[MAXLEN];
+        int n = in.read(buf);
+        return new String(buf, 0, n - 1);
+    }
+
+
+    public static void print(String txt) {
+        System.out.print(txt);
+    }
+
+    public static void println(String txt) {
+        print(txt + "\n");
+    }
+
+
+
+    public static int inputInt() throws IOException,
+            NumberFormatException {
+        return Integer.valueOf(inputString()).intValue();
+    }
+
+    public static int inputInt(String prompt) throws IOException,
+            NumberFormatException {
+        print(prompt);
+        return inputInt();
+    }
+
+    public static long inputLong() throws IOException,
+            NumberFormatException {
+        return Long.valueOf(inputString()).longValue();
+    }
+
+    public static long inputLong(String prompt) throws IOException,
+            NumberFormatException {
+        print(prompt);
+        return inputLong();
+    }
+
+    public static float inputFloat() throws IOException,
+            NumberFormatException {
+        return Float.valueOf(inputString()).floatValue();
+    }
+
+    public static float inputFloat(String prompt) throws IOException,
+            NumberFormatException {
+        print(prompt);
+        return inputFloat();
+    }
+
+    public static double inputDouble() throws IOException,
+            NumberFormatException {
+        return Double.valueOf(inputString()).doubleValue();
+    }
+
+    public static double inputDouble(String prompt)
+            throws IOException, NumberFormatException {
+        print(prompt);
+        return inputDouble();
+    }
+
+
+    public static char[] inputChars() throws IOException {
+        return (inputString()).toCharArray();
+    }
+
+    public static char[] inputChars(String prompt)
+            throws IOException {
+        print(prompt);
+        return (inputString()).toCharArray();
+    }
+}
